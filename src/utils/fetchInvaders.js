5020c1c4572e1c43891aa2b783f81069b5535f77
OGStefs <stefanistweg@gmail.com>
2022-07-01 15:43:40 +0200

    fixed scheduler typo

--- src/utils/fetchInvaders.js
+++ src/utils/fetchInvaders.js
@@ -2,7 +2,7 @@ import { schedule } from "node-cron";
 import { safeToFile } from "../utils/safeToFile.js";
 import invaderWallets from "../services/invaderWallets.js";
 
-schedule("*/30 * * * * *", async () => {
+schedule(" */30 * * * *", async () => {
   const owners = await invaderWallets();
   console.log(owners);
   const ownersWithTime = {
