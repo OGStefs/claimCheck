8c18cf2b3282f7ef0bcef9c23823bfeb463ca292
OGStefs <stefanistweg@gmail.com>
2022-07-11 20:53:06 +0200

    fixed web3 provider

--- src/utils/web3Init.js
+++ src/utils/web3Init.js
@@ -1,8 +1,8 @@
 import Web3 from "web3";
 
 export const web3 = new Web3(
-  // new Web3.providers.HttpProvider(process.env.ETHNODE)
-  new Web3.providers.WebsocketProvider(process.env.WS_ETHNODE)
+  new Web3.providers.HttpProvider(process.env.ETHNODE)
+  // new Web3.providers.WebsocketProvider(process.env.WS_ETHNODE)
 );
 
 //TODO: check blocktime against provider -> if difference too big, change provider
