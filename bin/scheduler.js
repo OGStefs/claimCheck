8bd5f6935e396b7c8b849dacc61605c518c963bf
OGStefs <stefanistweg@gmail.com>
2022-07-13 20:57:57 +0200

    updated heroku scheduler

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -1,8 +1,12 @@
 import invaderWallets from "../src/services/invaderWallets.js";
+import { fetchazukis } from "../src/utils/data/getAzukis.js";
+import { fetchlegends } from "../src/utils/data/getLegends.js";
 import { safeToFile } from "../src/utils/safeToFile.js";
 
 const getData = async () => {
   console.log("scheduler started");
+  await fetchazukis();
+  await fetchlegends();
   const owners = await invaderWallets();
   console.log(owners);
   safeToFile(owners);
